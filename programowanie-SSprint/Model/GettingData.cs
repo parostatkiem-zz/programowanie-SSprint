835406f706894cb12c0c968e24fc2158ff638eb5
Filip Strózik <filip.strozik@outlook.com>
2018-06-27 00:42:34 +0200

    dividing the model class into many files

---
+++ programowanie-SSprint/Model/GettingData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programowanie_SSprint
+{
+    partial class Model
+    {
+        public List<company> getAllCompanies()
+        {
+            return SSprintDataBase.companies.ToList();
+        }
+        public List<tshirt> getAllTshirts()
+        {
+            return SSprintDataBase.tshirts.ToList();
+        }
+
+        public List<color> getAllColors()
+        {
+            return SSprintDataBase.colors.ToList();
+        }
+
+        //todo : uzupełnić o więcej metod pobierania danych
+    }
+}
---
+++ programowanie-SSprint/Model/SearchingData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programowanie_SSprint
+{
+    partial class Model
+    {
+        public List<tshirt> findTshirtsByCompany(string name)
+        {
+            company companyName = SSprintDataBase.companies.FirstOrDefault(c => c.name == name);
+            return companyName.tshirts.ToList();
+        }
+        public List<tshirt> findTshirtsByColor(string name)
+        {
+            color colorName = SSprintDataBase.colors.FirstOrDefault(c => c.name == name);
+            return colorName.tshirts.ToList();
+        }
+        public List<tshirt> findTshirtsByStyle(string name)
+        {
+            style styleName = SSprintDataBase.styles.FirstOrDefault(c => c.name == name);
+            return styleName.tshirts.ToList();
+        }
+
+    }
+}
