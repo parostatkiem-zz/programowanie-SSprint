61760756f887a4da585308db86a41ebae5ac84c3
Jan Sudczak <jansudczak@gmail.com>
2018-07-03 20:52:01 +0200

    PushError method preparations

--- programowanie-SSprint/Views/ICommunicative.cs
+++ programowanie-SSprint/Views/ICommunicative.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace programowanie_SSprint
 {
+    public enum NotificationTypes { Information, Warning, Error};
     public interface ICommunicative
     {
+
+       // void PushNotification(string message, NotificationTypes type = NotificationTypes.Information, string duration="medium");
     }
 }
